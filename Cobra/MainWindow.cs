85481cfc9223c9084b217c975ee673543dd16a4c
Jordan Faulkner <jfaulkner2j@icloud.com>
2023-06-13 18:10:21 +0100

    added new cobra class with co-ordinate properties and constructor

--- Cobra/MainWindow.cs
+++ Cobra/MainWindow.cs
@@ -5,6 +5,23 @@ namespace Cobra
         public MainWindow()
         {
             InitializeComponent();
+            lblGameOver.Visible = false;
+        }
+        private void GameTimerEvent(object sender, EventArgs e)
+        { 
+
+        }
+        private void Restart()
+        {
+
+        }
+        private void EatItem()
+        {
+
+        }
+        private void GameOver()
+        {
+            lblGameOver.Visible = true;
         }
     }
 }
